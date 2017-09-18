8fc8652322bec0abfb4bbcf18f09be025d469101
pradeep <pradeep.budhathoki1234@gmail.com>
2017-09-18 13:24:20 +0545

    another commit

---
+++ PageNavigation_CrossPagePosting/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageNavigation_CrossPagePosting
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTransfer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNotPostBack_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("webform2.aspx");
+        }
+    }
+}
---
+++ PageNavigation_CrossPagePosting/WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageNavigation_CrossPagePosting
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           Page previouspage= Page.PreviousPage;
+            if(previouspage!=null && previouspage.IsCrossPagePostBack)
+            {
+                lblName.Text=((TextBox)previouspage.FindControl("txtName")).Text;
+                lblEmail.Text=((TextBox)previouspage.FindControl("txtEmail")).Text;
+            }
+            else
+            {
+                lblStatus.Text = "You landed on this page using a technique other than crosspagepostback";
+            }
+        }
+    }
+}
